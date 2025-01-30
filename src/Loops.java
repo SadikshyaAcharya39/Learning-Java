806da2ec4eb10648d74d0a674bc5216ae23835dc
SadikshyaAcharya39 <acharya.sadikshya33@gmail.com>
2025-01-30 22:38:46 +0545

    Added

--- src/Loops.java
+++ src/Loops.java
@@ -224,5 +224,351 @@ public class Loops {
 //            System.out.println(i);
 //        }
 
+        // Reversing a string
+//        String country = "USA";
+//        char[] array = country.toCharArray();
+//        for(int i = array.length - 1; i >= 0; i--){
+//            System.out.print(array[i]);
+//        }
+
+//        String reverse = "";
+//        for(int i = country.length() - 1; i >= 0; i--){
+//            reverse = reverse + country.charAt(i) ;
+//        }
+//        System.out.println(reverse);
+
+
+
+//        String name = "Shraddha";
+//        String reverse = "";
+//        for(int i = name.length() - 1; i >= 0; i--){
+//            reverse = reverse + name.charAt(i);
+//        }
+//        System.out.println(reverse);
+
+        // Reverse the number
+//        int num = 23456;
+//        int reverse =  0;
+//        for(; num!=0; num /= 10){
+//            int remainder = num % 10;
+//            reverse = reverse * 10 + remainder;
+//        }
+//        System.out.println(reverse);
+
+//        int num = 12323445;
+//        int count = 0;
+//        for(; num!=0; num /= 10){
+//            count++;
+//        }
+//        System.out.println(count);
+
+        // Count the number of words
+
+//        String word = "Mount Everest, known locally as Sagarmatha ";
+//        int wordCount = 0;
+//        for(int i = 0; i <= word.length(); i++){
+//            wordCount = word.split("\\s").length;
+//        }
+//        System.out.println(wordCount);
+
+        // Count digits in a number
+//        int num = 123123;
+//        int count = 0;
+//        for(; num!= 0; num/=10){
+//            count++;
+//        }
+//        System.out.println(count);
+
+        // Reverse a number
+
+//        int num = 123;
+//        int digit = 0;
+//        for(; num!=0; num /= 10){
+//            int remainder = num % 10;
+//            digit = digit * 10 + remainder;
+//        }
+//        System.out.println(digit);
+
+        // CCheck for palindrome
+
+//        int originalNum = 1221;
+//        int num = originalNum;
+//        int digit = 0;
+//        for(; num!=0; num /= 10){
+//            int remainder = num % 10;
+//            digit = digit * 10 + remainder;
+//        }
+//
+//        if(originalNum == digit){
+//            System.out.println(digit + " is palindrome");
+//        }
+//        else{
+//            System.out.println(digit + " is not a palindrome");
+//        }
+
+//        int originalNum = 1221;
+//        int num = originalNum;
+//        int digit = 0;
+//        for(; num!=0; num /= 10){
+//            int remainder = num % 10;
+//            digit = digit * 10 + remainder;
+//        }
+//
+//        if(originalNum == digit){
+//            System.out.println(digit + " is palindrome");
+//        }
+//        else{
+//            System.out.println(digit + " is not palindrome");
+//        }
+
+        // Sum of digits
+
+//        int num = 123;
+//        int sum = 0;
+//        for(; num != 0 ; num /= 10){
+//            int remainder = num % 10;
+//            sum += remainder;
+//        }
+//        System.out.println(sum);
+
+//        for(int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        Write programs to print the following patterns using loops:
+//        Right-angled triangle
+
+//        for(int i = 1; i <= 5; i++){
+//            for(int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        Write programs to print the following patterns using loops:
+//        Inverted Right-angled triangle
+
+//
+//        for(int row = 5; row >= 1; row--){
+//            for(int column = 1; column <= row; column++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        int rows = 5;
+//        for(int i = 1; i<= rows; i++){
+//
+//            for (int j = 1; j <= rows- i; j++) {
+//                System.out.print(" ");
+//            }
+//                for(int k = 1; k <= (2 * i - 1); k++){
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+//            }
+
+
+        /* Basic Loop Questions
+        Print Numbers from 1 to N: Write a program to print all numbers from 1 to a given number N.
+
+                Print Even Numbers: Print all even numbers between 1 and 100.
+
+        Sum of First N Numbers: Calculate the sum of the first N natural numbers.
+
+                Factorial of a Number: Compute the factorial of a given number N using a loop.
+
+                Multiplication Table: Print the multiplication table of a given number up to 10.
+
+        Count Digits in a Number: Write a program to count the number of digits in a given integer.
+
+                Reverse a Number: Reverse the digits of a given number using a loop.
+
+        Check for Palindrome: Determine if a given number or string is a palindrome (reads the same backward as forward).
+        */
+
+//        int num = 100;
+//        for(int i = 2; i <= num; i+=2){
+//            System.out.println(i);
+//        }
+
+//        int num = 5;
+//        int sum = 0;
+//        for(int i = 1; i <=num; i++){
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+
+//        int num = 3;
+//        int factorial = 1;
+//        for(int i = 1; i <= num; i++){
+//            factorial*= i;
+//        }
+//        System.out.println(factorial);
+
+//        int num = 5;
+//        for(int i = 1; i<= 10; i++){
+//            System.out.println(num + " * " + i + " = " + num*i);
+//        }
+
+//        int num = 123456;
+//        int count = 0;
+//        for(; num!=0; num/=10){
+//            count++;
+//        }
+//        System.out.println(count);
+
+// Count a char
+//        String name = "Sadikshya";
+//        int count = 0;
+//        for(int i = 0; i < name.length(); i++){
+//            count++;
+//        }
+//        System.out.println(count);
+
+        // Reversing a number
+
+//        int num = 1234;
+//        int digit = 0;
+//        for(; num!=0; num/=10){
+//            int remainder = num % 10;
+//            digit = digit * 10 + remainder;
+//        }
+//        System.out.println(digit);
+
+        // Reversing a string
+//        String name = "Sadikshya";
+//        char arr[] = name.toCharArray();
+//        for(int i = arr.length - 1; i >= 0; i--){
+//            System.out.print(arr[i]);
+//        }
+
+        // Sum of digits of number
+//        int num = 12345;
+//        int sum = 0;
+//        for(; num!=0; num /= 10){
+//            int remainder = num % 10;
+//            sum += remainder;
+//        }
+//        System.out.println(sum);
+
+        // Intermediate Loop
+
+        // Fibonacci Series
+
+//        int num = 10;
+//        int first = 0, second = 1;
+//
+//        for(int i = 3; i <= num; i++){
+//            int nextTerm = first + second;
+//            System.out.print(nextTerm + " , ");
+//            first = second;
+//            second = nextTerm;
+//        }
+
+
+        // Fibonacci Series
+//        int num = 10;
+//        int firstTerm = 1;
+//        int secondTerm = 2;
+//        System.out.print(firstTerm + "  " + secondTerm + "  ");
+//
+//        for(int i = 2; i <= num; i++){
+//            int nextTerm = firstTerm + secondTerm;
+//            System.out.print(nextTerm + "  ");
+//            firstTerm = secondTerm;
+//            secondTerm = nextTerm;
+//        }
+
+
+//        int num = 5;
+//        int firstTerm = 1, secondTerm = 2;
+//        System.out.print(firstTerm + " , " + secondTerm + " , ");
+//
+//        for(int i = 3; i <= num; i++){
+//            int nextTerm = firstTerm + secondTerm;
+//            System.out.print(nextTerm + " , ");
+//
+//            if(i == num){
+//                System.out.print(nextTerm);
+//            }
+//            else {
+//                System.out.print(nextTerm + " , ");
+//            }
+//            firstTerm = secondTerm;
+//            secondTerm = nextTerm;
+//        }
+
+
+//        int num  = 5;
+//        int firstTerm = 1, secondTerm = 2;
+//        System.out.print(firstTerm + " , " + secondTerm + " , ");
+//
+//        for(int i = 3; i <= num; i++){
+//           int nextTerm = firstTerm + secondTerm;
+//
+//           if(i == num){
+//               System.out.print(nextTerm);
+//           }
+//           else{
+//               System.out.print(nextTerm + " , ");
+//           }
+//
+//           firstTerm = secondTerm;
+//           secondTerm = nextTerm;
+//        }
+
+        // Fibonacci Series
+
+//        int num = 10;
+//        int firstTerm = 1, secondTerm = 2;
+//        System.out.print(firstTerm + " , " + secondTerm + " , ");
+//
+//        for(int i = 3; i <= num; i++){
+//            int nextTerm = firstTerm + secondTerm;
+//
+//            if(i == num){
+//                System.out.print(nextTerm);
+//            }
+//            else
+//            {
+//                System.out.print(nextTerm + " , ");
+//            }
+//
+//            firstTerm = secondTerm;
+//            secondTerm = nextTerm;
+//        }
+
+
+        // Check prime number
+
+        int num = 4;
+        boolean isPrime = false;
+
+        if(num == 0 || num == 1)
+        {
+          isPrime = false;
+        }
+
+        for(int i = 2; i <= num / 2; ++i) {
+            if(num % i == 0){
+                isPrime = true;
+                break;
+            }
+        }
+
+        if(!isPrime){
+            System.out.println(num + " is a prime number.");
+        }
+        else
+        {
+            System.out.println(num + " is not a prime number.");
+        }
+
+
     }
 }
---
+++ src/TernaryOperator.java
@@ -0,0 +1,13 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+//        int a = 10, b = 20;
+//        int a, b;
+//        a = b = 10;
+
+        int a = 10;
+        int b = 20;
+        String result =  (a == b)? "True" : "False";
+
+
+    }
+}
